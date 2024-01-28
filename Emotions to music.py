b72028e0d574221fc04b85499c45d63cb22cbd16
smitabita <102548137+smitabita@users.noreply.github.com>
2024-01-28 09:53:27 -0500

    Add files via upload

---
+++ Emotions to music.py
@@ -0,0 +1,13 @@
+pip pandas
+pip install spotipy
+
+"""import pandas as pd
+import numpy as np
+import spotify
+
+mood = "-"
+
+def getmusic(mood) :
+    if mood == "hello" :
+        print("hi!!")
+    return openaisuggested"""
